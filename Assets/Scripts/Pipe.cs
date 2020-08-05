928f299130ea1af1ee8137cb13d1136ed96bf66a
IFA <imamfh24@gmail.com>
2020-08-05 20:15:37 +0700

    First Commit

---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    //Global Variabel
+    [SerializeField] private Bird bird;
+    [SerializeField] private float speed = 1;
+
+    private void Update()
+    {
+        //Melakukan pengecekan jika burung belum mati
+        if (!bird.IsDead())
+        {
+            // Membuat pipa bergerak kesebalh kiri dengan kecepatan tertentu
+            transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+
+        if (bird)
+        {
+            // Mendapatkan komponent collider pada game object
+            Collider2D collider = GetComponent<Collider2D>();
+
+            // Melakukan pengecekan null variabel atau tidak
+            if (collider)
+            {
+                // Menonaktifkan collider
+                collider.enabled = false;
+            }
+            bird.Dead();
+        }
+    }
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Point : MonoBehaviour
+{
+    [SerializeField] Bird bird;
+    [SerializeField] int scorePoint = 1;
+    [SerializeField] private float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Melakukan pengecekan burung mati atau tidak
+        if (!bird.IsDead())
+        {
+            // Menggerakkan game object kesebelah kiri dengan kecepatan tertentu
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    public void SetSize(float size)
+    {
+        // Mendapatkan komponent BoxCollider2D
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+
+        // Pengecekan null variabel
+        if(collider != null)
+        {
+            // Mengubah ukuran collider sesuai dengan parameter
+            collider.size = new Vector2(collider.size.x, size);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Menempatkan komponen bird
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+        // Menambahkan score jika burung tidak null dan burung belum mati;
+        if(bird && !bird.IsDead())
+        {
+            bird.AddScore(scorePoint);
+        }
+    }
+}
